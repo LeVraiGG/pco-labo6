6d114c77cd9da44eaab90de096a8259a823e0767
LeVraiGG <tux.caroline@gmail.com>
2023-12-21 15:05:01 +0100

    clean

--- build-code-Desktop-Debug/labo6_gui/CMakeFiles/PCO_lab06_gui.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for PCO_lab06_gui.
-# This may be replaced when dependencies are built.
--- code/src/computeengine.cpp
+++
@@ -1,5 +0,0 @@
-#include "computeengine.h"
-
-int ComputeEngineA::nextId = 0;
-int ComputeEngineB::nextId = 0;
-int ComputeEngineC::nextId = 0;
